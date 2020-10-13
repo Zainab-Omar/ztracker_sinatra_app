acf04ff3747154ac4f3651b240e80132019e60fd
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2020-10-13 18:21:58 -0500

    create /signup route that direct users to create an account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
     
+    get '/signup' do
+        if !session[:user_id]
+            erb :'users/create_user'
+        else
+            redirect to ('/trackers')
+        end
+    end
+
+    post '/signup' do
+        
+    end
+
+    get '/signin' do
+        
+    end
+
+    post '/signin' do
+        
+    end
+
+    get '/signout' do
+        session.clear
+        redirect to ('/')
+    end
 
 end
