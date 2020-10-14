44e9380854d2ac1acb61b4fe9b0f01496bf35e75
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2020-10-13 19:14:03 -0500

    create /trackers route

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,3 +1,13 @@
 class TrackersController < ApplicationController
 
+    get '/trackers' do #index that show the user all trackers they create
+        if logged_in?
+            @trackers=current_user.trackers
+            erb :'trackers/index'
+        else
+            redirect to ('/signin')
+        end
+    end
+
+
 end
